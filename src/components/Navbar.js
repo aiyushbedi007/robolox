9bb5739ea0322005e62ab571f948dd279a0edb45
Aiyush Bedi <aiyushb@adobe.com>
2025-03-02 02:23:41 +0530

    fix: Update navbar brand name and remove dashboard link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Talent Directory</Link>
+                <Link className="navbar-brand" to="/">Robolox</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -25,9 +25,6 @@ const Navbar = () => {
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link className="nav-link" to="/talent-directory">Talent Directory</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/dashboard">Dashboard</Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/talent-check-in">Talent Check-In</Link>
