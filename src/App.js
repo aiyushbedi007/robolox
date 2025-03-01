a763ff699ad577ae34a8d68bd7eb80cdf3bab44e
Aiyush Bedi <aiyushb@adobe.com>
2025-03-02 00:32:06 +0530

    refactor: Remove unused logo and header from App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import TalentDirectory from './components/TalentDirectory';
 import EmployeeCard from './components/EmployeeCard';
@@ -14,20 +13,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
